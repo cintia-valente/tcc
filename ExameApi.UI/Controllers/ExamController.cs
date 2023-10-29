329b70a55877ef3e034307a2be1eac88dc377cc7
cintia-valente <cintiavalente@live.com>
2023-10-29 03:33:36 -0300

    feature: inicio do controller

--- ExameApi.UI/Controllers/ExamController.cs
+++ ExameApi.UI/Controllers/ExamController.cs
@@ -4,16 +4,16 @@ namespace ExameApi.UI.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class ExamController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<ExamController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public ExamController(ILogger<ExamController> logger)
         {
             _logger = logger;
         }
